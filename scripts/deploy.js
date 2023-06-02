5edd6bd9f0837c8fc93732560a69ee461e589c2e
talha073 <talhajavedd@gmail.com>
2023-06-02 15:30:36 +0400

    add deployment scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await ethers.getSigners();
+  const NAME = "Discord";
+  const SYMBOL = "DC";
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const Discord = await ethers.getContractFactory("Discord");
+  const discord = await Discord.deploy(NAME, SYMBOL);
+  await discord.deployed();
+  console.log("Contract deployed at: ", discord.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  //creating some channels
+  const CHENNAL_NAMES = ["General", "Intro", "Stuff", "Random"];
+  const COST = [tokens(1), tokens(0), tokens(0.25), tokens(0.002)];
+  for (let i = 0; i < 4; i++) {
+    const tx = await discord
+      .connect(deployer)
+      .createChanel(CHENNAL_NAMES[i], COST[i]);
+    await tx.wait();
+    console.log("created channel: ", CHENNAL_NAMES[i]);
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
