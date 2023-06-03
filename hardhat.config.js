daea8745b00bc2b7f8ac260141237bad3d027653
talha073 <talhajavedd@gmail.com>
2023-06-03 18:00:41 +0400

    add local network in hardhat congig file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+// require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  solidity: "0.8.3",
 };
